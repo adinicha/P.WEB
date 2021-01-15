19272042c2177cbc301148301f364adfd165a7b0
adinicha <aadindanisa@gmail.com>
2021-01-15 20:26:42 +0700

    progres terakhir menambah responsive dan melengkapi semua

---
+++ main.js
@@ -0,0 +1,26 @@
+let header = document.getElementsByTagName("header");
+
+if (document.title == "GoDestination") {
+    console.log("Masuk ke", document.title);
+    header[0].style.backgroundImage = "url(image/bener.jpg)";
+}
+else if (document.title == "Kawah Ijen") {
+    console.log("Masuk ke", document.title);
+    header[0].style.backgroundImage = "url(image/ijen5.jpg)";
+}
+else if (document.title == "Pink Beach") {
+    console.log("Masuk ke", document.title);
+    header[0].style.backgroundImage = "url(image/pinkbeach4.jpg)";
+}
+else if (document.title == "Gunung Bromo") {
+    console.log("Masuk ke", document.title);
+    header[0].style.backgroundImage = "url(image/bromo3.jpg)";
+}
+else if (document.title == "Air Terjun Nglirip") {
+    console.log("Masuk ke", document.title);
+    header[0].style.backgroundImage = "url(image/nglirip2.jpg)";
+}
+else if (document.title == "Candi Borobudur") {
+    console.log("Masuk ke", document.title);
+    header[0].style.backgroundImage = "url(image/candi1.jpg)";
+}
